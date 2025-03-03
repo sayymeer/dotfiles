f32bbf8d34a29ba53fccab62f1dc935484b4bb60
Sameer <sameer.yadv8@gmail.com>
2025-03-03 20:26:50 +0530

    moved to hyprland :

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -233,4 +233,27 @@ return {
 			"eslint_d",
 		},
 	},
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+		end,
+	},
 }
