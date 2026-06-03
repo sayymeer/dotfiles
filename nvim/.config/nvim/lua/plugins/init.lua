c68bf6566d6bd22e3e6a52410b9b3e71988e4bae
Sameer <sameer.yadv8@gmail.com>
2026-06-03 11:44:14 +0530

    removed lspsaga and improved lsp hover doc

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,90 +1,78 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    version = false,
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		version = false,
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "bash",
-        "c",
-        "cpp",
-        "diff",
-        "luadoc",
-        "markdown",
-        "go",
-        "rust",
-        "json",
-        "yaml",
-        "gitignore",
-        "dockerfile",
-        "python",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    },
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "nvimdev/lspsaga.nvim",
-    lazy = false,
-    config = function()
-      require("lspsaga").setup {}
-    end,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter", -- optional
-      "nvim-tree/nvim-web-devicons", -- optional
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"bash",
+				"c",
+				"cpp",
+				"diff",
+				"luadoc",
+				"markdown",
+				"go",
+				"rust",
+				"json",
+				"yaml",
+				"gitignore",
+				"dockerfile",
+				"python",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"lervag/vimtex",
+		ft = { "tex" },
+		init = function()
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_compiler_method = "latexmk"
+			vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:@tex @pdf"
+		end,
+	},
 
-  {
-    "lervag/vimtex",
-    ft = { "tex" },
-    init = function()
-      vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_compiler_method = "latexmk"
-      vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:@tex @pdf"
-    end,
-  },
-
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    -- no ft restriction so it attaches to LSP hover floating windows (markdown filetype)
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      file_types = { "markdown" },
-      render_modes = { "n", "c", "t" },
-      hover = { enabled = true },
-    },
-  },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		-- no ft restriction so it attaches to LSP hover floating windows (markdown filetype)
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			file_types = { "markdown" },
+			render_modes = { "n", "c", "t" },
+			hover = { enabled = true },
+		},
+	},
 }
